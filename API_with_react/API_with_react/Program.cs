7fcd223df864d3b71e5f223165da5f46c956c253
Fahrudeen Muhammadh Fayas <muhamadfayas8@gmail.com>
2025-04-30 15:52:36 +0530

    In program.cs
    i. Add //JSON Serializer // Add services with NewtonsoftJson
    
    ii. // Add CORS service

--- API_with_react/API_with_react/Program.cs
+++ API_with_react/API_with_react/Program.cs
@@ -6,6 +6,30 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+//JSON Serializer // Add services with NewtonsoftJson
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+    });
+
+// Add CORS service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+// Use CORS middleware
+
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
